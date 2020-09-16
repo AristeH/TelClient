0b5f57a3fe6f4c62ff3e93defd3e2ccbbd1303e8
schsv13 <schsv13@gmail.com>
2020-09-16 16:00:51 +0300

    Тест титла

---
+++ TelClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TelClient.ViewModels.Base;
+
+
+namespace TelClient.ViewModels
+{
+
+    class MainWindowViewModel:ViewModel
+    {
+        #region заголовок окна
+        /// <summary>Заголовок окна</summary>
+
+        private string _Title = "Телефоны";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+    }
+}
